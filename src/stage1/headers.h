9ea944354c5aee72d39f874f0e9acd1b3925e084
GeneralGuy4872 <45191270+GeneralGuy4872@users.noreply.github.com>
2018-11-19 23:39:09 -0600

    Create headers.h

---
+++ src/stage1/headers.h
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <math.h>
+#include <time.h>
+#include <complex.h>
+#define MAXBUFFER 2048
+
+//this program uses standard variable names for all it's input variables
+//foo bar baz quux garpley waldo plugh xyzzy grue yendor
+//it also uses the following names for internal variables
+//such as counters and allocators:
+//ninja, pirate, wizard, elf, valkyrie
+
+const double \u03C0 = M_PI;
+//unicode symbol for pi, or should be
+const double \u212f = M_E;
+//unicode symbol for e, or should be
+const float _Imaginary \u2148 = _Imaginary_I;
+//unicode symbol for i, or should be
+const float \u221e = INFINITY;
+//unicode symbol for infinity, or should be
+const char* myinput[MAXBUFFER];
+const 
+
+inline char[MAXBUFFER] mygets()
+	{
+	int* ninja[MAXBUFFER];
+
+	fgets(ninja, MAXBUFFER, stdin);
+	sscanf(ninja, "[^\r\n\v]", myinput);
+	return myinput;
+	};
+
+long mytertiation(double foo, unsigned int bar)
+	{
+	unsigned int ninja = 0;
+	long pirate = 1;
+	while (ninja < bar);
+		{
+		pirate = pow(foo, pirate);
+		};
+	return pirate;
+	};
+
+double mylog(long foo, double bar)
+	{
+	return (log(bar)/log(foo));
+	};
+
+double myroot(double foo, double bar)
+	{
+	return pow((1/foo), bar);
+	};
+
+unsigned long myfactorial(long foo)
+	{
+	long ninja = 0;
+	unsigned long pirate = 1;
+
+	while (ninja < foo)
+		{
+		pirate = pirate * (ninja + 1);
+				//lol
+		};
+	return pirate;
+	};
+
+unsigned long myderange(long foo)
+	{
+	return floor((myfactorial(foo)/M_E)+.5);
+	};
+
+unsigned long mynpr(unsigned long foo, unsigned long bar)
+	{
+	return (myfactorial(foo)/(myfactorial(foo-bar));
+	};
+
+unsigned long myncr(unsigned long foo, unsigned long bar)
+	{
+	return (myfactorial(foo)/myfactorial(bar)*(foo-bar));
+	};
+
+double mysec(double foo)
+	{
+	return (1/cos(foo));
+	};
+
+double mycot(double foo)
+	{
+	return (1/tan(foo));
+	};
+
+double mycsc(double foo)
+	{
+	return (1/sin(foo));
+	};
+
+double myarccot(double foo)
+	{
+	return (M_PI/2-arctan(foo));
+	};
+
+double myarcsec(double foo)
+	{
+	return (arccos(1/foo));
+	};
+
+double myarccsc(double foo)
+	{
+	return (arcsin(1/foo));
+	};
+
+
